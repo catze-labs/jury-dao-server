bd04d0590a3e731f35cc13455849d215373500ab
MG <coalsrbs7@gmail.com>
2023-04-14 17:49:19 +0900

    Add @UseGuards(JwtAuthGuard)

--- src/routes/nonce/nonce.controller.ts
+++ src/routes/nonce/nonce.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { createNonceResponse } from './responseSchema';
 import { CreateNonceDto } from '../dtos/createNonce.dto';
 import { NonceService } from '../../services/nonce/nonce.service';
+import { JwtAuthGuard } from '../../auth/jwt-auth-guard';
 
 @Controller('nonce')
 @ApiTags('Nonce')
@@ -11,6 +12,7 @@ export class NonceController {
 
   @Post()
   @ApiResponse(createNonceResponse)
+  @UseGuards(JwtAuthGuard)
   async createNonce(@Body() createNonceDto: CreateNonceDto) {
     const { walletAddress } = createNonceDto;
     const nonce: string = await this.nonceService.create(walletAddress);
