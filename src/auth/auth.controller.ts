e59fcb8911b81ee55933168a358abab8469d0e6d
MG <coalsrbs7@gmail.com>
2023-04-14 18:47:24 +0900

    Update login api url

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import {UserService} from 'src/services/user/user.service';
+import { UserService } from 'src/services/user/user.service';
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
-  constructor(private readonly userService : UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Post('/login')
+  @Post('')
   async login(@Body('walletAddress') walletAddress: string) {
-	  return await this.userService.login(walletAddress)
+    return await this.userService.login(walletAddress);
   }
 }
