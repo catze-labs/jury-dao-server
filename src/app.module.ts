376c7f776c91548b6cafc328d73eadade458aa53
aiiiden <hjjam100@gmail.com>
2023-04-14 08:40:37 +0000

    Fix jwt error

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ServicesModule } from "./services/services.module";
 import { RoutesModule } from './routes/routes.module';
-import {AuthModule} from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [AuthModule, ServicesModule, RoutesModule],
+  imports: [ConfigModule.forRoot({
+	cache : true,
+	isGlobal : true
+  }),AuthModule, ServicesModule, RoutesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import {JuryService} from './jury/jury.service';
 import {UserService} from './user/user.service';
+import {JwtService} from '@nestjs/jwt';
+
 @Module({
-  providers: [PrismaService, JuryService, UserService],
+  providers: [PrismaService, JuryService, UserService, JwtService],
   exports: [PrismaService, JuryService, UserService],
 })
 export class ServicesModule {}
