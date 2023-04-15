b6a88511e55f96262e037717b21334c55f306fc3
MG <coalsrbs7@gmail.com>
2023-04-15 14:54:13 +0900

    Add signatureDto in register api

--- src/routes/user/user.controller.ts
+++ src/routes/user/user.controller.ts
@@ -1,17 +1,10 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UserService } from '../../services/user/user.service';
 import { JwtAuthGuard } from '../../auth/jwt-auth-guard';
 import { RegisterUserDto } from '../dtos/registerUser.dto';
-import {getUser} from 'src/decorators/getUser.decorator';
+import { getUser } from 'src/decorators/getUser.decorator';
+import { SignatureDto } from '../dtos/signature.dto';
 
 @Controller()
 @ApiTags('User')
@@ -19,7 +12,15 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async register(@Body() registerUserDto: RegisterUserDto) {
+  async register(
+    @Body() registerUserDto: RegisterUserDto,
+    @Body() signatureDto: SignatureDto,
+  ) {
+    const { walletAddress, signature } = signatureDto;
+    const userAddress: string | undefined =
+      await this.signatureService.getAddress(walletAddress, signature);
+    this.signatureService.validateUserWalletAddress(userAddress, walletAddress);
+
     const { walletAddress, name, email, twitterHandle } = registerUserDto;
     return await this.userService.register(
       walletAddress,
