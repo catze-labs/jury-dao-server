bb726294eb013db54b6112f336d8f777c7dc250f
MG <coalsrbs7@gmail.com>
2023-04-15 14:39:37 +0900

    Reformat code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthDto } from './auth.dto';
-import {AuthService} from './auth.service';
+import { AuthService } from './auth.service';
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
-  constructor(private readonly authService : AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('')
   async login(@Body() authDto: AuthDto) {
     const { walletAddress } = authDto;
-    return await this.authService.login(walletAddress);
+    return this.authService.login(walletAddress);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,33 +1,30 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import {PrismaService} from 'src/services/prisma/prisma.service';
-import {UserService} from 'src/services/user/user.service';
-import {JwtService} from '@nestjs/jwt'
+import { PrismaService } from 'src/services/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
-	constructor(private readonly jwtService : JwtService, private readonly prismaService : PrismaService){}
+  public async login(walletAddress: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        walletAddress,
+      },
+    });
 
-	public async login(walletAddress : string) {
+    if (!user) {
+      throw new NotFoundException({
+        message: 'User not found',
+      });
+    }
 
-		const user = await this.prismaService.user.findUnique({
-  		    where: {
-        		walletAddress,
-      		    },
-    		});
+    const payload = { ...user };
+    const jwt = this.jwtService.sign(payload);
 
-    		if (!user) {
-      			throw new NotFoundException({
-        			message: 'User not found',
-      			});
-    		}
-
-    		const payload = { ...user };
-    		const jwt = this.jwtService.sign(payload);
-
-    		return { accessToken: jwt };
-	}
-
-
-
-
+    return { accessToken: jwt };
+  }
 }
