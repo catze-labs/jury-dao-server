9b46786caa8d158c7587663ef8a0ead38033d22a
aiiiden <hjjam100@gmail.com>
2023-04-15 15:09:39 +0900

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import {ValidationPipe} from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+  	origin : ["/^http://localhost:[0-9]{1,}/", "https://jury-dao-web.vercel.app"]
+  })
 
   app.useGlobalPipes(new ValidationPipe({
 	transform : true,
