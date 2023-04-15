a0fe642c4868da1752d9d248b9e5406301ed5186
aiiiden <hjjam100@gmail.com>
2023-04-15 18:32:08 +0900

    Add auth response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { AuthDto } from './auth.dto';
 import { AuthService } from './auth.service';
+import {authResponse} from './responseSchema';
+
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('')
+  @ApiResponse(authResponse)
   async login(@Body() authDto: AuthDto) {
     const { walletAddress } = authDto;
     return this.authService.login(walletAddress);
---
+++ src/auth/responseSchema.ts
@@ -0,0 +1,10 @@
+export const authResponse = {
+	status : 201,
+	description  : "Get accessToken",
+	schema : {	
+		properties : {
+			accessToken : {type : 'string'}
+		}
+	}
+
+}
